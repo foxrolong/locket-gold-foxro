fa9f62e599d157e14e1a8cc99dbf860d6deb1acc
FoxRO long <longjakiu1@gmail.com>
2026-07-18 06:51:56 +0700

    Update Locket.js

--- js/Locket.js
+++ js/Locket.js
@@ -13,7 +13,7 @@ var locket02 = {
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
   period_type: "normal",
-  expires_date: "2099-12-18T01:04:17Z",
+  expires_date: "2026-7-17T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
   original_purchase_date: "2024-07-28T01:04:18Z",
@@ -25,7 +25,7 @@ var foxrolong = {
   grace_period_expires_date: null,
   purchase_date: "2024-07-28T01:04:17Z",
   product_identifier: "com.locket02.premium.yearly",
-  expires_date: "2099-12-18T01:04:17Z"
+  expires_date: "2026-7-17T01:04:17Z"
 };
 
 const match = Object.keys(mapping).find(e => ua.includes(e));
