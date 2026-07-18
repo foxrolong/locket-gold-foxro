a67c2f33d73e6d3943965464ad827ee3b068605c
FoxRO long <longjakiu1@gmail.com>
2026-07-18 07:10:58 +0700

    Update Locket.js

--- js/Locket.js
+++ js/Locket.js
@@ -16,14 +16,14 @@ var locket02 = {
   expires_date: "2027-7-17T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2026-06-17T01:04:18Z",
-  purchase_date: "2026-06-17T01:04:17Z",
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
   store: "app_store"
 };
 
 var foxrolong = {
   grace_period_expires_date: null,
-  purchase_date: "2026-06-17T01:04:17Z",
+  purchase_date: "2024-07-28T01:04:17Z",
   product_identifier: "com.locket02.premium.yearly",
   expires_date: "2027-7-17T01:04:17Z"
 };
